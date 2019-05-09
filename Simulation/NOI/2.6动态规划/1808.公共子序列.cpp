f2b0205148e44b7835d337b83c6db578a8868c30
lzw429 <SHUYIHENG2015XIN>
2019-05-09 18:54:55 +0800

    Create 1808.公共子序列.cpp

---
+++ Simulation/NOI/2.6动态规划/1808.公共子序列.cpp
@@ -0,0 +1,74 @@
+//
+// Created by syh on 19-5-9.
+//
+
+/*
+ * 1808:公共子序列
+查看 提交 统计 提问
+总时间限制: 1000ms 内存限制: 65536kB
+描述
+我们称序列Z = < z1, z2, ..., zk >是序列X = < x1, x2, ..., xm >的子序列当且仅当存在 严格上升 的序列< i1, i2, ..., ik >，使得对j = 1, 2, ... ,k, 有xij = zj。比如Z = < a, b, f, c > 是X = < a, b, c, f, b, c >的子序列。
+
+现在给出两个序列X和Y，你的任务是找到X和Y的最大公共子序列，也就是说要找到一个最长的序列Z，使得Z既是X的子序列也是Y的子序列。
+输入
+输入包括多组测试数据。每组数据包括一行，给出两个长度不超过200的字符串，表示两个序列。两个字符串之间由若干个空格隔开。
+输出
+对每组输入数据，输出一行，给出两个序列的最大公共子序列的长度。
+样例输入
+abcfbc         abfcab
+programming    contest
+abcd           mnp
+样例输出
+4
+2
+0
+来源
+翻译自Southeastern Europe 2003的试题
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int maxn = 210;
+int dp[maxn][maxn]; // dp[i][j] 表示字符串1的前i个字符和字符串2的前j个字符的最长公共子序列的长度
+
+/**
+ * 动态规划数组初始化
+ */
+void init() {
+    for (int i = 0; i < maxn; i++) {
+        dp[0][i] = dp[i][0] = 0;
+    }
+}
+
+
+/**
+ * 求公共子序列长度
+ * @param str1 字符串1
+ * @param str2 字符串2
+ * @return str1 和 str2 的公共子序列长度
+ */
+int lcs(string str1, string str2) {
+    init();
+    for (int i = 1; i <= str1.size(); ++i) {
+        for (int j = 1; j <= str2.size(); ++j) {
+            if (str1[i - 1] == str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[str1.size()][str2.size()];
+}
+
+int main() {
+    string str1, str2;
+    while (cin >> str1) {
+        cin >> str2;
+        cout << lcs(str1, str2) << endl;
+    }
+    return 0;
+}
--- Simulation/王道论坛计算机考研机试指南/7.4Coincidence【最长公共子序列LCS】.cpp
+++ Simulation/王道论坛计算机考研机试指南/7.4Coincidence【最长公共子序列LCS】.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cstring>
 #include <iostream>
+
 #define MAXL 105
 using namespace std;
 int dp[MAXL][MAXL];
@@ -8,24 +9,24 @@ int dp[MAXL][MAXL];
 // 分别组成的两个前缀字符串的最长公共子串长度
 
 int main() {
-  char s1[MAXL], s2[MAXL];
-  while (cin >> s1 >> s2) {
-    int len1 = strlen(s1);
-    int len2 = strlen(s2);
-    for (int i = 0; i < len1; i++)
-      dp[i][0] = 0;
-    for (int j = 0; j < len2; j++)
-      dp[0][j] = 0;
-    for (int i = 1; i <= len1; i++) {
-      for (int j = 1; j <= len2; j++) {
-        if (s1[i - 1] != s2[j - 1]) {
-          // 字符数组下标从 0 开始，第 i 个字符位置为 s1[i-1]
-          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-        } else
-          dp[i][j] = dp[i - 1][j - 1] + 1;
-      }
+    char s1[MAXL], s2[MAXL];
+    while (cin >> s1 >> s2) {
+        int len1 = strlen(s1);
+        int len2 = strlen(s2);
+        for (int i = 0; i < len1; i++)
+            dp[i][0] = 0;
+        for (int j = 0; j < len2; j++)
+            dp[0][j] = 0;
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (s1[i - 1] != s2[j - 1]) {
+                    // 字符数组下标从 0 开始，第 i 个字符位置为 s1[i-1]
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                } else
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+        }
+        cout << dp[len1][len2] << endl;
     }
-    cout << dp[len1][len2] << endl;
-  }
-  return 0;
+    return 0;
 }
