4cde16ac0c5e6ca7ba78e2c02b84fcfeeabd21b2
lzw429 <SHUYIHENG2015XIN>
2019-05-10 08:30:32 +0800

    update

---
+++ Simulation/NOI/2.6动态规划/1775.采药.cpp
@@ -0,0 +1,37 @@
+//
+// Created by syh on 19-5-10.
+//
+
+
+// 本题是 01 背包裸题
+#include <iostream>
+
+using namespace std;
+
+const int maxn = 110; // 最大草药数
+const int maxt = 1010; // 最大时间
+int t[maxn];
+int v[maxn];
+int dp[maxt];
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int T, M; // 总时间和草药数目
+    cin >> T >> M;
+    for (int i = 0; i < M; i++) {
+        cin >> t[i] >> v[i];
+    }
+
+    // 01背包
+    for (int i = 0; i < M; i++) {
+        for (int j = T; j - t[i] >= 0; j--) {
+            // cout << i << " " << j << " " << dp[j] << endl;
+            dp[j] = max(dp[j], dp[j - t[i]] + v[i]);
+        }
+    }
+
+    cout << dp[T] << endl;
+    return 0;
+}
---
+++ Simulation/NOI/2.6动态规划/1944.吃糖果.cpp
@@ -0,0 +1,25 @@
+//
+// Created by syh on 19-5-10.
+//
+
+#include <iostream>
+
+using namespace std;
+
+const int maxn = 25;
+int dp[maxn];
+
+int main() {
+    int n;
+    cin >> n;
+
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i < maxn; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    cout << dp[n] << endl;
+    return 0;
+}
